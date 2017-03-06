9d6acd780d3b48899ae841b6de32063f49d7ba25
Patrick <pbentley07@gmail.com>
2017-03-06 00:16:57 -0500

    Added more detail to Results in Panel

--- logic.js
+++ logic.js
@@ -31,9 +31,10 @@ var urlBase =  "https://api.nytimes.com/svc/search/v2/articlesearch.json";
           $('#wellSection').append(wellSection);
 
           $('#article-'+i).append('<h3>'+NYTData.response.docs[i].headline.main +'</h3>');
-          $('#article-'+i).append('<a>'+NYTData.response.docs[i].web_url +'</a>');
+          $('#article-'+i).append('<h5>'+NYTData.response.docs[i].snippet +'</h5>');
+          $('#article-'+i).append("<a href=" + NYTData.response.docs[i].web_url + ">"+ NYTData.response.docs[i].web_url + "</a>");
           $('#article-'+i).append('<h5>'+NYTData.response.docs[i].section_name +'</h5>');
-          $('#article-'+i).append('<h5>'+NYTData.response.docs[i].pub_date+'</h5>');
+          $('#article-'+i).append('<h5>'+ NYTData.response.docs[i].pub_date+'</h5>');
 
         }
         console.log(urlBase);
